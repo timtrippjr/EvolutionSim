d42f56c9189515a67742341224d12f6c95777f96
timtrippjr <timothytrippjr@gmail.com>
2026-08-19 18:11:13 -0400

    conditional branching, add raylib-cs!

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Hello, World!");
+
+string? theStupidestPerson = Console.ReadLine(); 
+
+bool reachedFailLine = false;
+if (theStupidestPerson is null)
+reachedFailLine = true;
+if (theStupidestPerson == string.Empty)
+reachedFailLine = true;
+
+
+if (reachedFailLine)
+Console.WriteLine("im feeling facty today.");
+else
+Console.WriteLine("wow! i HATE "+theStupidestPerson);
+
+
+Console.ReadKey(); 
